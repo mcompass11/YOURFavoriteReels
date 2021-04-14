7b1d1bb853f003d30e52ee72781b4c579b7c679e
Monkendrick <52892395+mcompass11@users.noreply.github.com>
2021-04-14 13:58:04 -0400

    implemented environment variables

--- index.js
+++ index.js
@@ -231,4 +231,5 @@ app.listen(port, '0.0.0.0' , () => {
     console.log('Listening on Port ' + port);
 });
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true});
