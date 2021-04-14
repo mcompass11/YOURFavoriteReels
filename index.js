45f681b869d63c043aab36e46e17b76f20401f78
Monkendrick <52892395+mcompass11@users.noreply.github.com>
2021-04-14 12:44:08 -0400

    moved the mongoose/api integration to correct file

--- index.js
+++ index.js
@@ -230,3 +230,5 @@ const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0' , () => {
     console.log('Listening on Port ' + port);
 });
+
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true});
--- models.js
+++ models.js
@@ -6,10 +6,6 @@ const Users = Models.User;
 
 const bcrypt = require('bcrypt');
 
-mongoose.connect('mongodb://localhost:27017/test', {
-    useNewUrlParser: true, useUnifiedTopology: true
-});
-
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
     Description: {type: String, require: true},
