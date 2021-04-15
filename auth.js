da1f6626ff2e07e6182cc7666ee8d611820a442c
Monkendrick <52892395+mcompass11@users.noreply.github.com>
2021-04-15 06:24:46 -0400

    minor update

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 const jwtSecret = 'your_jwt_secret'; //This has to be the same key used in the JWTStrategy
 const jwt = require('jsonwebtoken'),
     passport = require('passport');
-require('./passport'); //Your local passport file
+require('./passport.js'); //Your local passport file
 
 let generateJWTToken = (user) => {
     return jwt.sign(user, jwtSecret, {
