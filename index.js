9be763468bcb3324417b1ffbc2698ee10946c960
Monkendrick <52892395+mcompass11@users.noreply.github.com>
2021-05-15 08:14:52 -0400

    removed duplicate code

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const Users = Models.User;
 
 const cors = require('cors');
 
-let allowedOrigins = ['http://localhost:8080', 'https://yourfavoritereels.herokuapp.com', 'http://localhost:1234'];
+// let allowedOrigins = ['http://localhost:8080', 'https://yourfavoritereels.herokuapp.com', 'http://localhost:1234'];
 
 app.use(cors({
     origin: (origin, callback) => {
