c693fefbbaeaf33d52ae601d3aa4535fce3f9f0b
Monkendrick Williams <monkendrickwilliams@MonkendricksMBP.lan>
2023-10-18 10:22:32 -0400

    edited CORS permission

--- index.js
+++ index.js
@@ -19,10 +19,25 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'https://heroku.com'];
+
 app.use(cors({
-  origin: '*'
-}
-));
+    origin: (origin, callback) => {
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        //if a specific origin isn't found on the list
+        let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+        return callback(new Error(message ), false);
+      }
+      return callback(null, true);
+    }
+  }));
+
+// app.use(cors({
+//   origin: '*'
+// }
+// ));
 
 require('./auth.js')(app);
 
