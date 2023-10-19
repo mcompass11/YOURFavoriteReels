86bada6a0b3fa90e3eddea47da2e7ab346b6bf9d
Monkendrick Williams <monkendrickwilliams@MonkendricksMBP.lan>
2023-10-19 11:32:16 -0400

    added way to check connection error with MongoDB

--- index.js
+++ index.js
@@ -48,7 +48,9 @@ app.use(express.static('public'));
 
 // mongoose.connect('mongodb+srv://mcompass11:March_22_2023@monkendrickdb.vsqnr.mongodb.net/?retryWrites=true&w=majority',
 //   { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB: ', err));
 
 
 /**
@@ -56,7 +58,7 @@ mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifie
  * @method GET
  * @returns {string}
  */
-app.get('/', (res) => {
+app.get('/', (req, res) => {
     res.send('Welcome to movie gallery!');
 });
 
